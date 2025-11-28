897f7e3af898d982b548c8520444ae7eb1061603
SideeID <sandipratama164026@gmail.com>
2025-11-28 12:41:44 +0700

    update formatDate utils untuk format indo + waktu created at

--- frontend/utils/formatDate.ts
+++ frontend/utils/formatDate.ts
@@ -5,14 +5,22 @@ export const formatDate = (dateString: string): string => {
   const diffInMs = now.getTime() - date.getTime();
   const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 
+  const formatTime = (dateString: string): string => {
+    const hour = new Date(dateString).getHours();
+    const minute = new Date(dateString).getMinutes();
+    return `${hour.toString().padStart(2, '0')}:${minute
+      .toString()
+      .padStart(2, '0')}`;
+  }
+
   if (diffInDays === 0) {
-    return 'today';
+    return 'hari ini pada ' + formatTime(dateString);
   } else if (diffInDays === 1) {
-    return 'yesterday';
+    return 'kemarin pada ' + formatTime(dateString);
   } else if (diffInDays < 7) {
-    return `${diffInDays} days ago`;
+    return `${diffInDays} hari yang lalu pada ` + formatTime(dateString);
   } else {
-    return date.toLocaleDateString('en-US', {
+    return date.toLocaleDateString('id-ID', {
       year: 'numeric',
       month: 'short',
       day: 'numeric',
